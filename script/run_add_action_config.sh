c18e98ee52a49cb27e8fc282a8c0e0bcc0dc3551
server <you@example.com>
2026-06-29 13:14:23 +0800

    chore(script): 修改动作配置脚本的输出文件路径
    将输出文件名从episodes_lingbot.jsonl更改为episodes.jsonl，统一数据集文件命名

--- script/run_add_action_config.sh
+++ script/run_add_action_config.sh
@@ -2,4 +2,4 @@
 
 python tools/add_action_config.py \
     --episodes_file /home/jszn/hewu/dataset/lerobot_put_screw_into_the_hole_20260519/meta/episodes_origin.jsonl \
-    --output_file /home/jszn/hewu/dataset/lerobot_put_screw_into_the_hole_20260519/meta/episodes_lingbot.jsonl
+    --output_file /home/jszn/hewu/dataset/lerobot_put_screw_into_the_hole_20260519/meta/episodes.jsonl
